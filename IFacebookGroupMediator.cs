e022eca56974d55c068515e0aecc297429208c3c
chien <chiennguyen.170921@gmail.com>
2023-05-18 21:17:50 +0700

    update

--- IFacebookGroupMediator.cs
+++ IFacebookGroupMediator.cs
@@ -3,7 +3,7 @@ namespace MediatorDesignPattern
     //Mediator Interface
     public interface IFacebookGroupMediator
     {
-         void SendMessage(string msg, User user);
+        void SendMessage(string msg, User user);
         void RegisterUser(User user);
     }
 }
